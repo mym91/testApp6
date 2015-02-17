b46967494c706fc2e90231508a7ec1dc9278b4f6
Myriam Koch <myrikoch@hotmail.com>
2015-02-17 16:10:47 +0100

    change controller.js map

--- testApp6/www/js/controllers.js
+++ testApp6/www/js/controllers.js
@@ -8,14 +8,37 @@ angular.module('starter.controllers', [])
 		//alert(fileSystem.root.name);
 	}, null);
 	*/
-	document.getElementById("info").innerHTML = '1<img src="/map/16/34477/42572.png" alt="Bild" />' ;
+	document.getElementById("info").innerHTML = '2<img src="/map/16/34477/42572.png" alt="Bild" />' ;
 	
 	var map = L.map('map');
 	
 	//document.getElementById("info").innerHTML = cordova.file.applicationDirectory+'www/map/';
 	L.tileLayer('/map/{z}/{x}/{y}.png',{
 		maxZoom: 18  
-	}).addTo(map);				
+	}).addTo(map);			
+				
+		var map = L.map('map-canvas').setView([45.423, -75.679], 13);
+		//this works, but is online:
+		/*
+		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 18
+		}).addTo(map);
+		*/
+		//TODO build something to fall back to web if not found.
+		L.tileLayer('/map/{z}/{x}/{y}.png', {
+			maxZoom: 17
+		}).addTo(map);
+		
+		L.marker([45.423, -75.679]).addTo(map).bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+		
+		var popup = L.popup();
+		function onMapClick(e) {
+			popup
+			.setLatLng(e.latlng)
+			.setContent("You clicked the map at " + e.latlng.toString())
+			.openOn(map);
+		}
+		map.on('click', onMapClick);
 })
 	
 
